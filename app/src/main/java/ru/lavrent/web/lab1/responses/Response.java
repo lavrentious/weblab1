48f6f5a1fb770a718895e31b302ddcee9af51098
lavrentious <lavrent0@yandex.ru>
2024-09-16 18:52:07 +0300

    feat: return current time

--- app/src/main/java/ru/lavrent/web/lab1/responses/Response.java
+++ app/src/main/java/ru/lavrent/web/lab1/responses/Response.java
@@ -4,9 +4,11 @@ import ru.lavrent.web.lab1.Main;
 
 public class Response {
   private long scriptTime;
+  private long time;
 
   public Response() {
     this.scriptTime = (System.nanoTime() - Main.startTime) / 1000000;
+    this.time = System.currentTimeMillis();
   }
 
   public long getScriptTime() {
